83a0d50f5d9f7211f2f6659539d560796db771b1
raphaelthief <97448225+raphaelthief@users.noreply.github.com>
2025-03-27 14:15:32 +0100

    Update subdomain.py

--- Dependencies/subdomain.py
+++ Dependencies/subdomain.py
@@ -1,80 +1,115 @@
-import requests, json
-
+import requests, json, time
+from bs4 import BeautifulSoup
 from colorama import init, Fore, Style
-from datetime import datetime
+
 
 init()  # Init colorama for colored text in terminal
 
 
-import requests
-from colorama import Fore
+def detect_login_page(content):
+    login_keywords = ["login", "connexion", "password", "sign in", "authentication"]
+    soup = BeautifulSoup(content, "html.parser")
 
-def fetch_json(url):
-    """Performs a GET request and returns the JSON data or an error message."""
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Checks for HTTP errors
-        try:
-            return response.json()  # Returns the JSON response
-        except requests.exceptions.JSONDecodeError:
-            print(f"{Fore.RED}[!] Error : Invalid JSON response from {url}")
-            print(f"{Fore.RED}[!] Raw response content : {response.text}")
-            return None
-    except requests.exceptions.RequestException as e:
-        print(f"{Fore.RED}[!] Request error to {url} : {e}")
-        return None
+    if soup.title and any(keyword in soup.title.text.lower() for keyword in login_keywords):
+        return True
 
+    if soup.find("input", {"type": "password"}) or soup.find("form", {"action": lambda x: x and "login" in x.lower()}):
+        return True
+
+    return False
+
+
+# Target subdomains enum
 def subreponse(domain):
-    """Récupère et affiche les sous-domaines depuis leakix.net."""
-    url = f"https://leakix.net/api/subdomains/{domain}"
-    subdomains = fetch_json(url)
+    
+    headers = {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36"
+    }    
+    
+    url = f"https://crt.sh/?q={domain}"
+    try:
 
-    if subdomains:
-        print(f"\n{Fore.YELLOW}[!] Subdomains")
-        print(f"{Fore.YELLOW}[?] Source : {Fore.GREEN}leakix.net{Fore.YELLOW}")
+     
+        
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+    except requests.exceptions.RequestException as e:
+        print(f"{M}[Error] {R}Request error to {url} : {e}")
+        return
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    
+    
+    try:
+        table = soup.find_all('table')[1] 
+        rows = table.find_all('tr')[1:]  
+    except IndexError:
+        print(f"{M}[Error] {R}No valid data found on {url}")
+        return    
+
+    certificates = {}
+    for row in rows:
+        cols = row.find_all('td')
+        if len(cols) >= 6:
+            cert_id = cols[0].text.strip()
+            logged_at = cols[1].text.strip()
+            not_before = cols[2].text.strip()
+            not_after = cols[3].text.strip()
+            common_name = cols[4].text.strip()
+
+            if common_name not in certificates or logged_at > certificates[common_name]['logged_at']:
+                certificates[common_name] = {
+                    "cert_id": cert_id,
+                    "logged_at": logged_at,
+                    "not_before": not_before,
+                    "not_after": not_after,
+                    "common_name": common_name
+                }
+
+    if not certificates:
+        print(f"{Fore.MAGENTA}[!] {Fore.GREEN}No subdomains found for {Fore.YELLOW}{domain}")
+        return
+
+    print(f"{Fore.MAGENTA}[!] {Fore.GREEN}Subdomains for {Fore.YELLOW}{domain}")
+    print(f"{Fore.MAGENTA}[?] {Fore.GREEN}Source : {Fore.YELLOW}crt.sh")
+    print("-" * 50)
+    
+    for cert in certificates.values():
+        test_url = f"http://{cert['common_name']}"  # HTTP default        
+        try:
+            
+            test_response = requests.get(test_url, headers=headers, timeout=5)
+            status = test_response.status_code
+            statuscode = test_response.status_code
+            statusV = "/"
+            if status == 403 or status == 200:
+                if detect_login_page(test_response.text):
+                    status = f"login page [{Fore.RED}{statuscode}{Fore.GREEN}]"            
+            
+        except requests.exceptions.Timeout as e:
+            status = f"Timedout [{Fore.RED}{statuscode}{Fore.GREEN}]"
+
+            test_url = "/"
+        except requests.exceptions.ConnectionError as e:
+            if "getaddrinfo failed" in str(e):
+                status = f"DNS resolution failed [{Fore.RED}{statuscode}{Fore.GREEN}]"
+
+            else:
+                status = f"Connection error [{Fore.RED}{statuscode}{Fore.GREEN}]"
+
+            test_url = "/"
+        except requests.exceptions.RequestException as e:
+            status = f"Unexpected error [{Fore.RED}{statuscode}{Fore.GREEN}]"
+
+            test_url = "/"
+            
+        print(f"{Fore.GREEN}[+] {Fore.YELLOW}Common Name    : {Fore.CYAN}{cert['common_name']}")
+        print(f"{Fore.GREEN}[+] {Fore.YELLOW}Logged At      : {Fore.GREEN}{cert['logged_at']}")
+        print(f"{Fore.GREEN}[+] {Fore.YELLOW}More Infos     : {Fore.GREEN}https://crt.sh/?id={cert['cert_id']}{Fore.YELLOW}")
+        print(f"{Fore.GREEN}[+] {Fore.YELLOW}Satus          : {Fore.GREEN}{status}{Fore.YELLOW}")
+        
+
+        print(f"{Fore.GREEN}[+] {Fore.YELLOW}Direct URL     : {Fore.GREEN}{test_url}{Fore.YELLOW}")
         print("-" * 50)
-        for entry in subdomains:
-            print(f"{Fore.GREEN}[+] {Fore.YELLOW}Subdomain    : {Fore.CYAN}{entry.get('subdomain', 'N/A')}")
-            print(f"{Fore.GREEN}[+] {Fore.YELLOW}Distinct IPs : {Fore.GREEN}{entry.get('distinct_ips', 'N/A')}")
-            print(f"{Fore.GREEN}[+] {Fore.YELLOW}Last Seen    : {Fore.GREEN}{entry.get('last_seen', 'N/A')}{Fore.YELLOW}")
-            print(f"-" * 50)
-
-    # Appelle la seconde fonction pour obtenir les sous-domaines de crt.sh
-    subreponse2(domain)
-
-def subreponse2(domain):
-    """Récupère et affiche les sous-domaines depuis crt.sh."""
-    url = f"https://crt.sh/json?q={domain}"
-    certs = fetch_json(url)
-
-    if certs:
-        filtered_results = {}
-        for cert in certs:
-            common_name = cert.get("common_name")
-            entry_date = cert.get("entry_timestamp", "").split("T")[0]
-
-            if common_name and entry_date:  # Vérifiez que les données sont présentes
-                if (
-                    common_name not in filtered_results or
-                    entry_date > filtered_results[common_name]["entry_timestamp"].split("T")[0]
-                ):
-                    filtered_results[common_name] = {
-                        "common_name": common_name,
-                        "entry_timestamp": cert.get("entry_timestamp", "N/A"),
-                        "id": cert.get("id", "N/A")
-                    }
-
-        # Affichez les résultats filtrés
-        filtered_list = list(filtered_results.values())
-        if not filtered_list:
-            print(f"{Fore.YELLOW}[!] Aucun certificat trouvé pour le domaine {domain}.")
-            return
-
-        print(f"\n{Fore.YELLOW}[!] Subdomains")
-        print(f"{Fore.YELLOW}[?] Source : {Fore.GREEN}crt.sh{Fore.YELLOW}")
-        print("-" * 50)
-        for cert in filtered_list:
-            print(f"{Fore.GREEN}[+] {Fore.YELLOW}Name       : {Fore.CYAN}{cert['common_name']}")
-            print(f"{Fore.GREEN}[+] {Fore.YELLOW}Entry time : {Fore.GREEN}{cert['entry_timestamp']}")
-            print(f"{Fore.GREEN}[+] {Fore.YELLOW}More infos : {Fore.GREEN}https://crt.sh/?id={cert['id']}{Fore.YELLOW}")
-            print("-" * 50)
+        
+        time.sleep(0.5)
